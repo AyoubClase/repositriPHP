a06d3a5f347889d83e75605df03ee6f5022c02fb
AyoubClase <102050173+AyoubClase@users.noreply.github.com>
2022-04-04 11:40:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$apiKey = "e2c2408a6bbdc7d4a3af53ecfb8bb140";
+$cityId = "Madrid";
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+?>
